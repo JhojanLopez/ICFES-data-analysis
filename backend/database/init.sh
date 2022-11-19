3ff43ac2874c087c7b094296fe72f8b71eee2675
jhohan.lopez <jhohan.lopez@inter-telco.com>
2022-11-19 03:16:33 -0500

    cambios en la inicializacion de la db

--- backend/database/init.sh
+++ backend/database/init.sh
@@ -1,2 +1,2 @@
 #datos de inicializacion , collections: data --> data_valle.csv
-mongoimport -d icfes_valle -c data --type=csv --file /app/data_valle.csv --headerline
+mongoimport -d icfes_valle -c estudiante --type=csv --file /app/data_valle.csv --headerline
